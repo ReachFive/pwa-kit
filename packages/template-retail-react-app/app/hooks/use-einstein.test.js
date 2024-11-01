ed034cf8d7721959c28131957890b9234b87575b
Jang ho Jung <jangho.jung@salesforce.com>
2024-11-01 11:46:12 -0700

    Add dntPreference to existing tests

--- packages/template-retail-react-app/app/hooks/use-einstein.test.js
+++ packages/template-retail-react-app/app/hooks/use-einstein.test.js
@@ -19,7 +19,8 @@ import fetchMock from 'jest-fetch-mock'
 const einsteinApi = new EinsteinAPI({
     host: `http://localhost/test-path`,
     einsteinId: 'test-id',
-    siteId: 'test-site-id'
+    siteId: 'test-site-id',
+    dntPreference: false
 })
 
 const fetchOriginal = global.fetch
