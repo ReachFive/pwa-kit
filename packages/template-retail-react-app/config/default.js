25b3661110ed176c674c11a2f276f04895bf8ba9
Yuna Kim <84923642+yunakim714@users.noreply.github.com>
2024-12-04 16:13:36 -0500

    @W-16889880 - Complete Social Login Integration: Connect Backend API to UI (#2124)
    Integrate Social Login UI changes in the PWA Kit to backend SLAS API calls.
    ---------
    
    Signed-off-by: Yuna Kim <84923642+yunakim714@users.noreply.github.com>

--- packages/template-retail-react-app/config/default.js
+++ packages/template-retail-react-app/config/default.js
@@ -21,8 +21,9 @@ module.exports = {
                 callbackURI: 'https://webhook.site/27761b71-50c1-4097-a600-21a3b89a546c'
             },
             social: {
-                enabled: false,
-                idps: ['google', 'apple']
+                enabled: true,
+                idps: ['google', 'apple'],
+                redirectURI: '/social-callback'
             }
         },
         defaultSite: 'RefArchGlobal',
